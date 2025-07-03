1a832f5a3215fe3a87654f316e6db3f557d6c375
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-07-03 07:42:18 +0530

    sumOfDigits

---
+++ Basic_Introduction/basic_problems/addOfTwoDigit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int a = 10;
+int b = 20;
+
+cout<<a+b<<endl;
+
+// update numbers and add one variable to store that values in that variable
+
+a = 40;
+b = 90;
+
+int c = a + b;
+
+cout<<"updated value : "<< c <<endl;
+    return 0;
+}
