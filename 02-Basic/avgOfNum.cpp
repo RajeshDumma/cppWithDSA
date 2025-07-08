f64d5e947710049b1722c9cf7bf3c41479954f66
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-07-08 14:45:28 +0530

    Average of numbers

---
+++ 02-Basic/avgOfNum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a , b , c , d;
+    cout<<"Enter the numbers : "<<endl;
+    cin >> a >> b >> c;
+    
+
+     d = (a+b+c)/3.0 ;
+
+    cout<< d << endl;
+    return 0;
+
+}
