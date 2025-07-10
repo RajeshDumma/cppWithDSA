42e3c853412f6579a03a30c1616c590b2740147f
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-07-10 16:11:01 +0530

    Relational operation

---
+++ Operators/relationalOpe.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+// Relational operators
+int main(){
+
+     int a , b;
+     cout<<"Enter the a value: "<<endl;
+     cin>> a;
+     cout<<"Enter the b value: "<<endl;
+     cin>> b;
+     cout<<(a == b)<<endl; // 0
+     cout<<(a != b)<<endl; // 1
+     cout<<(a > b)<<endl; // 0
+     cout<<(a < b)<<endl; // 1
+     cout<<(a >= b)<<endl; // 0
+     cout<<(a <= b)<<endl; // 1
+   return 0;
+}
