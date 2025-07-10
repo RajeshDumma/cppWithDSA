9c32ed2031e7951621cc4395f3646b81f8233fd4
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-07-10 09:59:44 +0530

    basic problems

---
+++ 02-Basic/swapTwoNum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a , b ;
+    cout<<"Enter the numbers: "<<endl;
+    cin>> a >> b;
+    cout<<"Before swap "<<endl;
+    cout<<"a : "<<a<<", b : " <<b<<endl;
+    a = b - a; // a = 10 , b = 2
+    b = b - a;
+    a = a + b;
+     cout<<"After swap "<<endl;
+    cout<<"a : "<<a<<", b : " <<b<<endl;
+
+    return 0;
+
+}
