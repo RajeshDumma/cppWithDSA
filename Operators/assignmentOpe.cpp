05a719edebcc149342326d2381932bef16d42268
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-07-18 14:51:54 +0530

    Assignment operator

---
+++ Operators/assignmentOpe.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cout<<"Enter a: "<<endl;
+    cin>> a;
+    cout<<"Enter b: "<<endl;
+    cin>>b;
+    a += b;
+     cout<<a<<endl;
+    a -= b;
+     cout<<a<<endl;
+    a *= b;
+     cout<<a<<endl;
+    a /= b;
+    cout<<a<<endl;
+     a %= b;
+    cout<<a<<endl;
+    return 0;
+}
