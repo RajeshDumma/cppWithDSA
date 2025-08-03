61006b12875d8f254a231e33853a71080b68a234
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-08-04 04:57:58 +0530

    colour match

---
+++ Conditional_Statements/P005.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x1, x2, y1, y2;
+    cout<<"Enter values: "<<endl;
+    cin>> x1 >> y1 >> x2 >> y2;
+
+    if((x1+y1)%2 == 0 && (x2+y2)%2 == 0) cout<<"YES"<<endl;
+   else if((x1+y1)%2 != 0 && (x2+y2)%2 != 0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
