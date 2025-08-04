a5b567772c068963295798544e35e1759df02a16
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-08-04 11:59:29 +0530

    problem

--- Conditional_Statements/P005.cpp
+++ Conditional_Statements/P005.cpp
@@ -7,7 +7,7 @@ int main(){
     cin>> x1 >> y1 >> x2 >> y2;
 
     if((x1+y1)%2 == 0 && (x2+y2)%2 == 0) cout<<"YES"<<endl;
-   else if((x1+y1)%2 != 0 && (x2+y2)%2 != 0) cout<<"YES"<<endl;
+    else if((x1+y1)%2 != 0 && (x2+y2)%2 != 0) cout<<"YES"<<endl;
     else cout<<"NO"<<endl;
     return 0;
 }
