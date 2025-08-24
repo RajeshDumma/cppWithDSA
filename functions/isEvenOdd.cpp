7e9e658d4a70de6b799811f8be04e5fe150c9ddb
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-08-24 10:14:44 +0530

    even_odd_fun

--- functions/isEvenOdd.cpp
+++ functions/isEvenOdd.cpp
@@ -13,3 +13,28 @@ int main(){
     isEvenOdd(num);
     return 0;
 }
+
+
+/*
+// Write a function isEven(int n) that returns true if the number is even, otherwise false.
+
+
+#include <iostream>
+
+using namespace std;
+
+bool isEven(int);
+
+int main(){
+    int x ;
+    cout << "Enter the number : "<<endl;
+    cin >> x;
+    
+    cout << (isEven(x) ? "false" : "true" )<< endl;
+    
+}
+
+bool isEven(int a){
+    return (a%2) ;
+}
+*/
