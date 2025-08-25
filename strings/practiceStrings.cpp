56f54c4ca2acac0ecf4a163896bd3ed4f09719d9
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-08-25 14:29:46 +0530

    stringBasic

---
+++ strings/practiceStrings.cpp
@@ -0,0 +1,14 @@
+
+// basic strings 
+#include <iostream>
+using namespace std;
+
+int main(){
+    string str  = "welcome";
+ //   cin >> str ;  // here is a problem for ex:- "hello world" but output will be "hello" only
+
+    string str2;
+    getline(cin, str2);
+//    cout <<" string 1 " << str << endl;
+    cout << str2 <<endl;
+}
