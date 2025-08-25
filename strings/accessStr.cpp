0c7b7cbba19559108479712e915214335b9bf16a
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-08-25 14:40:38 +0530

    strings

---
+++ strings/accessStr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+   string str = "RajeshDumma"; // indexing value start at 0 , here 0 to 10
+   cout << str[5] << str[2] << str[1] << str[0] << str[10] <<endl;
+
+   // 
+    return 0;
+}
--- strings/practiceStrings.cpp
+++ strings/practiceStrings.cpp
@@ -4,11 +4,16 @@
 using namespace std;
 
 int main(){
-    string str  = "welcome";
+  //  string str  = "welcome";
  //   cin >> str ;  // here is a problem for ex:- "hello world" but output will be "hello" only
 
     string str2;
     getline(cin, str2);
 //    cout <<" string 1 " << str << endl;
     cout << str2 <<endl;
+
+    // string can declaration 
+   // char[] stri = "system language";
+   // string strg = "programming language";
+ //   string stgn("cpp language");
 }
